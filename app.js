eeaa649058293811f6d90f974cbfe8cfe2cc1374
eliasescalante <eliasw.escalante@gmail.com>
2025-06-09 10:03:06 -0300

    update to app.js

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 /* Servidor */
 import express from "express";
 import { json } from "sequelize";
-
+import cors from "cors";
+import dotenv from "dotenv";
+import fileUpload from "express-fileupload";
 
 
 const app = express();
 const PORT = 8080;
 
+// Configuración de CORS
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
+// Middleware para manejar la subida de archivos
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+}));
 
 //Rutas
 app.listen(PORT, () => {
